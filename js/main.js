aeffa6f8939c1d21a62b0de0e1f705b01b1a274a
gitkrax <jonne.arjoranta@gmail.com>
2017-01-22 16:02:16 +0200

    Translate to Finnish

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var grammar = tracery.createGrammar(autoflaneur);
 
 function generateText() {
-	document.getElementById("generatedText").innerHTML = grammar.flatten('#origin');
+	document.getElementById("generatedText").innerHTML = grammar.flatten('#aloitus');
 }
